0bab0539caea780f2a6302bc5ba04e341cf885ba
Asthor Bjornsson <asthor02@gmail.com>
2020-08-08 02:55:38 +0200

    allow herokuapp.com

--- src/try_django/settings.py
+++ src/try_django/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'jd66nrgtu!kify(k0a!o^wf9wiffluskb3ha9&tgfv1mi^5xjo')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", ".herokuapp.com"]
 
 LOGIN_URL = ''
 # Application definition
