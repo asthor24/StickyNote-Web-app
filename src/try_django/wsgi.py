330981a10dd6d1b936085b594f474437d815ffb4
Asthor Bjornsson <asthor02@gmail.com>
2020-08-08 19:08:36 +0200

    just trying out whatever at this point tbh

--- src/try_django/wsgi.py
+++ src/try_django/wsgi.py
@@ -12,6 +12,6 @@ import try_django
 from django.core.wsgi import get_wsgi_application
 
 print("starting from here so its finding the wsgi.py file")
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'try_django.settings')
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'try_django.settings')
 
 application = get_wsgi_application()
