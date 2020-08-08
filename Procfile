4879427c7427d873d7a55816d54de3aa4270b17c
Asthor Bjornsson <asthor02@gmail.com>
2020-08-08 19:04:29 +0200

    afaskdfjla

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn try_django.wsgi:app
+web: gunicorn try_django.wsgi
--- src/try_django/wsgi.py
+++ src/try_django/wsgi.py
@@ -11,6 +11,7 @@ import os
 import try_django
 from django.core.wsgi import get_wsgi_application
 
+print("starting from here so its finding the wsgi.py file")
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'try_django.settings')
 
 application = get_wsgi_application()
